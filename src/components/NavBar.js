6bfd311e6a9f5992269d1b71257b48889512cf5f
Emiliano Duarte <emilianojduarte@gmail.com>
2022-03-15 22:25:01 -0300

    Cambios y decoración
    Se raelizan ajustes en en navbar para que se ajuste un poco más al proyecto planificado.
    Se agregan algunos estilos para sumar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Button from '@mui/material/Button';
-
+import './NavBar.css';
 
 function NavBar() {
     return(
         <header className="mainHeader">
             <div className="mainHeader__containerLogo">
-                <img src="logo192.png" className="mainHeader_containerLogo--logo" alt="logo"/>
+                <img src="logo.png" className="mainHeader_containerLogo--logo" alt="logo"/>
+            </div>
+            <div>
+                <p className="mainHeader__brand">TechAnt</p>
             </div>
             <ul className="navbar">
-                <li><Button variant="outlined">Home</Button></li>
-                <li><Button variant="outlined">Productos</Button></li>
-                <li><Button variant="outlined">Nosotros</Button></li>
+                <li><Button variant="outlined" color="primary">Categorías</Button></li>
+                <li><Button variant="outlined">Ofertas</Button></li>
                 <li><Button variant="outlined">Contacto</Button></li>
+                <li><Button variant="outlined">Carrito</Button></li>
+                <li><Button variant="outlined">Cuenta</Button></li>
             </ul>
         </header>
     )
