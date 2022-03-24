7a64fc35c49d9721ad33aa0a454af3c542db6f44
Emiliano Duarte <emilianojduarte@gmail.com>
2022-03-24 15:29:17 -0300

    CSS en CartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './CartWidget.css';
 
 function CartWidget({contador}){
     return(
         <Button color="secondary">
                 <ShoppingCartIcon/>
-                <span>{contador}</span>
+                <span className="badge">{contador}</span>
         </Button>
     )
 }
