d3af264d30f4d28a914ac6ea60430a893f9211ee
Emiliano Duarte <emilianojduarte@gmail.com>
2022-03-27 17:51:08 -0300

    Creación de ItemDetail

---
+++ src/ItemDetail/ItemDetail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './ItemDetail.css';
+
+function ItemDetail (){
+
+}
+
+export default ItemDetail;
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,7 +1,19 @@
 import React from "react";
 import './ItemDetailContainer.css';
 
+
 function ItemDetailContainer(){
+    const item = {
+        id: 3,
+        type: "ram",
+        url: "./assets/images/ram3.jpg",
+        enviogratis: true,
+        recomendado: true,
+        price: 16108,
+        stock: 16,
+        description: "2x16GB Kingston Fury Beast DDR4"
+    }
+
     return(
         <div className="mainItemDetailContainer">
             <h2>Productos Seleccionado</h2>
