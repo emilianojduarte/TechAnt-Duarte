495d2d54fee03bb22670f8bf95caecfd3f4dcf7f
Emiliano Duarte <emilianojduarte@gmail.com>
2022-04-07 00:40:41 -0300

    update aún con errores

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,29 +1,63 @@
 //Componentes
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import ItemCount from "../ItemCount/ItemCount";
-
+import Button from '@mui/material/Button';
 
 function ItemDetail ({item}){
     const {url, price, stock, description, detail} = item;
     const [contador, setContador] = useState(0);
+    const [mostrarItemCount, setMostrarItemCount] = useState(true);
 
     const onAdd = (e, count) => {
-        setContador(count)
+        e.stopPropagation()
+        setContador(count);
         console.log("el valor de contador es:", contador);
-        //por qué no me funciona bien? -Revisar
+        setMostrarItemCount(false);
     }
+
+    const mostraContenido = () => {
+        if (mostrarItemCount === true){
+        console.log("mostrar cual es true")
+        return(
+            <div className="mainItemDetail">
+                <div className="mainItemDetail__img">
+                    <img src={url} alt="foto del producto"></img>
+                </div>
+                <div className="mainItemDetail__details">
+                    <p>{description}</p>
+                    <p>Precio : $ {price}</p>
+                    <p>{detail}</p>
+                    <ItemCount stock={stock} initial={1} action={onAdd}/>
+                </div>
+            </div>
+        )
+    }
+    else{
+        console.log("mostrar cual es false")
+        return(
+            <div className="mainItemDetail">
+                <div className="mainItemDetail__img">
+                    <img src={url} alt="foto del producto"></img>
+                </div>
+                <div className="mainItemDetail__details">
+                    <p>{description}</p>
+                    <p>Precio : $ {price}</p>
+                    <p>{detail}</p>
+                    <Button>Finalizar Compra</Button>
+                </div>
+            </div>
+        )
+    }
+    }
+    useEffect(()=>{
+        mostraContenido();
+    },[mostrarItemCount])
+
     return(
-        <div className="mainItemDetail">
-            <div className="mainItemDetail__img">
-                <img src={url} alt="foto del producto"></img>
-            </div>
-            <div className="mainItemDetail__details">
-                <p>{description}</p>
-                <p>Precio : $ {price}</p>
-                <p>{detail}</p>
-                <ItemCount stock={stock} initial={1} action={onAdd}/>
-            </div>
-        </div>
+        <>
+        {mostraContenido}
+        </>
+        
     )
 }
 
