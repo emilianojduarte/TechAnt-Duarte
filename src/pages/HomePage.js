b13e4a32f6129b0d55e7d86a47f3d2cb44c664bd
Emiliano Duarte <emilianojduarte@gmail.com>
2022-04-28 22:42:19 -0300

    Corrección de mediaquery
    Se corrige media query para mayor consistencia.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,48 +7,11 @@ import '@splidejs/react-splide/css';
 import './HomePage.css';
 
 function HomePage (){
-    const isMobile = useMediaQuery('(min-width:760px)');
+    const breakPointScreen = useMediaQuery('(max-width:760px)');
+    const portraitScreen = useMediaQuery('(orientation: portrait)');
     return(
         <>
-        {isMobile?(
-            <div>
-            <section>
-                <Splide aria-label="Carrousel"
-                    options={{
-                        width: '100vw',
-                        autoplay: true,
-                        type  : 'fade',
-                        rewind: true,
-                        height: '95vh',
-                    }}
-                >
-                    <div className="splide__float">
-                        <p>Catálogo de Productos</p>
-                    </div>
-                    <SplideSlide>
-                        <img src="./assets/images/carrousel1.jpg" alt="carrousel 1"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img src="./assets/images/carrousel2.jpg" alt="carrousel 2"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img src="./assets/images/carrousel3.jpg" alt="carrousel 3"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img src="./assets/images/carrousel4.jpg" alt="carrousel 4"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img src="./assets/images/carrousel5.jpg" alt="carrousel 5"/>
-                    </SplideSlide>
-            </Splide>
-            </section>
-            <h1>Bienvenidos a la tienda</h1>
-            <div>
-                <h2>otro placerholder</h2>
-            </div>
-            </div>
-        ):(
-            <div>
+        {breakPointScreen&&portraitScreen ?(
             <section>
                 <Splide aria-label="Carrousel"
                     options={{
@@ -79,13 +42,39 @@ function HomePage (){
                     </SplideSlide>
                 </Splide>
             </section>
-            <h1>Bienvenidos a la tienda</h1>
-            <div>
-                <h2>otro placerholder</h2>
-            </div>
-            </div>
+        ):(
+            <section>
+                <Splide aria-label="Carrousel"
+                    options={{
+                        width: '100vw',
+                        autoplay: true,
+                        type  : 'fade',
+                        rewind: true,
+                        height: '95vh',
+                    }}
+                >
+                    <div className="splide__float">
+                        <p>Catálogo de Productos</p>
+                    </div>
+                    <SplideSlide>
+                        <img src="./assets/images/carrousel1.jpg" alt="carrousel 1"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src="./assets/images/carrousel2.jpg" alt="carrousel 2"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src="./assets/images/carrousel3.jpg" alt="carrousel 3"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src="./assets/images/carrousel4.jpg" alt="carrousel 4"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src="./assets/images/carrousel5.jpg" alt="carrousel 5"/>
+                    </SplideSlide>
+                </Splide>
+            </section>
         )}
-        
+        <h1>Bienvenidos a la tienda</h1>
         </>
     )
 }
